df815154eb91cea6a4320449407c833c6d2749dc
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-10-19 16:59:12 -0400

    added socials

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -3,6 +3,29 @@ const Socials = () => {
     <>
     <div className="socials-container">
       <h2 className="socials-heading">Socials</h2>
+
+      <ul className="socials">
+
+<li>
+
+    <a href="https://www.linkedin.com/in/catherine-hilton-61105b224" target="_blank" rel="noreferrer"
+        className="bi bi-linkedin"></a>
+
+</li>
+
+<li>
+    <i>
+        <a href="https://github.com/cat-thebeesknees" target="_blank" rel="noreferrer" className="bi bi-github"></a>
+    </i>
+</li>
+<i>
+
+    <li><a href="https://www.discordapp.com/users/JustcallmeCat#4877" target="_blank" rel="noreferrer"
+            className="bi bi-discord"></a>
+    </li>
+</i>
+
+</ul>
     </div>
     </>
   );
