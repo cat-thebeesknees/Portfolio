db7fafdce399ad6347cbe6ac55f33f1d45a73d63
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-10-19 20:21:14 -0400

    added all the element files I think I'm going to use on my home page navbar, may add more later but this is the starting point

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,12 @@
+import "../Styles/Projects.css";
+const Projects = () => {
+  return (
+    <>
+    <div className="projects-container">
+    Projects
+    </div>
+    </>
+  )
+}
+
+export default Projects
--- src/Components/RoutePaths.jsx
+++ src/Components/RoutePaths.jsx
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom"; 
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Projects from "./Projects";
+import Resume from "./Resume";
+import NotFound from "./NotFound";
+import "../Styles/RoutePaths.css";
 const RoutePaths = () => {
   return (
   <>
-  <div className="route-container">RoutePaths</div>
+  <div className="route-container">
+<div id="route-section">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/resume" element={<Resume />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</div>
+
+  </div>
   </>
   );  
 };
