4243c7c6d11590325690b1617872b67b7f9c2441
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-10-21 17:27:57 -0400

    added a photo wheel to the homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,21 +3,29 @@ import Socials from "./Socials";
 
 import "../Styles/Home.css";
 import Header from "./Header";
+import Profile from "./Me";
 
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        Home
         <div id="logo-img">
-        <Header />
-         
+          <Header />
         </div>
         <div id="socials-container">
           <Socials />
         </div>
         <div id="nav-container">
           <NavBar />
+          <h1 className="home-heading">Catherine Hilton</h1>
+          <Profile />
+
+          <h3 className="home-subheading">Software Development Engineer</h3>
+          <p className="home-paragraph">
+            &ldquo; Remember that when you take your first step into the life of
+            your dreams, the first thing to meet you there will be fear. Nod.
+            Keep walking.&rdquo;
+          </p>
         </div>
       </div>
     </>
