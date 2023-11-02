2d0b3a747065e4ab59fe94dacaea24b843db5a27
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-11-02 15:43:38 -0400

    added header and navbar to the contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,17 @@
 import "../Styles/Contact.css";
+import Header from "./Header";
+import NavBar from "./NavBar";
 
 const Contact = () => {
   return (
-    <> <div className="contact-container">Contact</div>
-    </>
+    <>
+    <div className="contact-container">
+      <h2 className="contact-heading">Contact Me</h2>
+    <Header />
+    
+    <NavBar />
+    </div>
+  </>
   );
 };
 
