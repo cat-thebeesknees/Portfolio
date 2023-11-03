844cb67b43a77ce63f9043c704cbb06213a57855
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-11-03 09:52:43 -0400

    added a contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,6 +10,13 @@ const Contact = () => {
     <Header />
     
     <NavBar />
+
+    <form>      
+  <input name="name" type="text" className="feedback-input" placeholder="Name" />   
+  <input name="email" type="text" className="feedback-input" placeholder="Email" />
+  <textarea name="text" className="feedback-input" placeholder="Comment"></textarea>
+  <input type="submit" value="SUBMIT"/>
+</form>
     </div>
   </>
   );
