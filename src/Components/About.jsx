2b27760dac06ffa58934402eee856552a07799ab
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-11-10 15:04:17 -0500

    fixed about page up, added new photos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,7 @@
 import "../Styles/About.css";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import Profile from "./Me";
 
 
 const About = () => {
@@ -11,6 +12,22 @@ const About = () => {
       <Header />
       
       <NavBar />
+      <Profile />
+      <p className="personal-quote">
+      &ldquo;When we seek to discover the best in others, we somehow bring out the best in ourselves.&rdquo;
+      William Arthur Ward
+      </p>
+
+      <p className="about-paragraph">
+      Okay, let us just take a moment to acknowledge how uncomfortable it is to write an &ldquo;about me&rdquo; paragraph.<br/>
+       Now, what I often hear people say about me is that I am magnetic.<br />
+       I naturally see the posititve side of things, and believe in the goodness of others.<br />
+       I love when people feel comfortable sharing their perspectives, it&apos;s an opportunity to understand concepts and solutions I didn&apos;t consider.<br /> 
+       Offering uncritical acceptance, leaves room to build trust and learn from one another.<br />
+      Just imagine what can be accomplished when we have access to the unfiltered knowledge of everyone on the team.<br />
+       I&apos;ve learned to better handle critisim. If it stings a bit to hear it, I know it&apos;s because there&apos;s truth to it.<br />
+      
+      </p>
       </div>
     </>
   );
--- src/Components/Me.jsx
+++ src/Components/Me.jsx
@@ -2,55 +2,44 @@
 import Me from "../Images/me.jpg";
 import Flowers from "../Images/flowers.jpg";
 import yellow from "../Images/yellow.jpg";
+import Dog from "../Images/dog.jpg";
+import Kids from "../Images/kids.jpg";
+import Ember from "../Images/ember.jpg";
 // Import result is the URL of your image
-import  "../Styles/Me.css";
+import "../Styles/Me.css";
 
 console.log(Me);
 
 function Profile() {
-  // Import result is the URL of your image
   return (
     <>
-<div className="gallery">
-<span style={{'--i': 1}}>
-  <img id="me-img" src={Me} alt="Me" />
-  </span>
-  <span style={{'--i': 2}}>
-  <img id="flowers-img" src = {Flowers} alt="Flowers" />
-  </span>
-  <span style={{'--i': 3}}>
-  <img id="yellow-img" src = {yellow} alt="yellow" />
-  </span>
-  <span style={{'--i': 4}}>
-  <img id="me-img" src={Me} alt="Me" />
-  </span>
-  <span style={{'--i': 5}}>
-  <img id="flowers-img" src = {Flowers} alt="Flowers" />
-  </span>
-    <span style={{'--i': 6}}>
-  <img id="yellow-img" src = {yellow} alt="yellow" />
-  </span>
-    <span style={{'--i': 7}}>
-  <img id="me-img" src={Me} alt="Me" />
-  </span>
-    <span style={{'--i': 8}}>
-  <img id="flowers-img" src = {Flowers} alt="Flowers" />
-  </span>
-</div>
-
-
-
-
-
-
-  
-
-  
-
-    
-
-
-  </>
+      <div className="gallery">
+        <span style={{ "--i": 1 }}>
+          <img id="me-img" src={Me} alt="Me" />
+        </span>
+        <span style={{ "--i": 2 }}>
+          <img id="dog-img" src={Dog} alt="Dog" />
+        </span>
+        <span style={{ "--i": 3 }}>
+          <img id="kids-img" src={Kids} alt="Kids" />
+        </span>
+        <span style={{ "--i": 4 }}>
+          <img id="flowers-img" src={Flowers} alt="Flowers" />
+        </span>
+        <span style={{ "--i": 5 }}>
+          <img id="ember-img" src={Ember} alt="Ember" />
+        </span>
+        <span style={{ "--i": 6 }}>
+          <img id="yellow-img" src={yellow} alt="yellow" />
+        </span>
+        <span style={{ "--i": 7 }}>
+          <img id="me-img" src={Me} alt="Me" />
+        </span>
+        <span style={{ "--i": 8 }}>
+          <img id="kids-img" src={Kids} alt="Kids" />
+        </span>
+      </div>
+    </>
   );
 }
 
