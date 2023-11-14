131fe4e45b137acbdd30eedf2ceb12954533abc3
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-11-13 21:43:58 -0500

    landing page is complete for now

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,13 +5,14 @@ import NavBar from "./NavBar";
 const Contact = () => {
   return (
     <>
-    <div className="contact-container">
-      <h2 className="contact-heading">Contact Me</h2>
+    
+      
     <Header />
     
     <NavBar />
-
-    <form>      
+    <div className="contact-container">
+    <form>  
+    <h2 className="contact-heading">Contact Me</h2>    
   <input name="name" type="text" className="feedback-input" placeholder="Name" />   
   <input name="email" type="text" className="feedback-input" placeholder="Email" />
   <textarea name="text" className="feedback-input" placeholder="Comment"></textarea>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,33 +1,32 @@
 import NavBar from "./NavBar";
-
+import Cartoon from "../Images/cartoonme.jpg";
 
 import "../Styles/Home.css";
 import Header from "./Header";
 
-
 const Home = () => {
   return (
     <>
-    <Header />
-    <NavBar />
+      <Header />
+      <NavBar />
       <div className="home-container">
-       
-        
         <div className="wrapper">
           <div className="bg"> Catherine Hilton</div>
+
           <div className="fg"> Catherine Hilton </div>
         </div>
 
-       
-        
-      
-        <h3 className="home-subheading">Software Development Engineer</h3>
-        <p className="home-paragraph">
-          &ldquo; Remember that when you take your first step into the life of
-          your dreams, the first thing to meet you there will be fear. Nod. Keep
-          walking.&rdquo;
-        </p>
+        <img id="cartoon-img" src={Cartoon} alt="Cartoon" />
       </div>
+      <h3 className="home-subheading">Software Developer</h3>
+      <p className="home-paragraph">
+        &ldquo; Remember that when you take your first step into the life of
+        your dreams, the first thing to meet you there will be fear.
+        <br />
+        Nod.
+        <br />
+        Keep walking.&rdquo;
+      </p>
     </>
   );
 };
