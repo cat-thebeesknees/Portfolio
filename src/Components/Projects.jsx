03f217f6d86e8ba8862b0b694e649e0887decace
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2024-06-21 08:56:06 -0400

    added links to project page

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,18 +2,19 @@ import Header from "./Header";
 
 import NavBar from "./NavBar";
 import "../Styles/Projects.css";
-
+import react from "../assets/react.png";
+import mcj from "../assets/mcj.png";
+import paint from "../assets/paint.png";
+import list from "../assets/list.png";
+import first from "../assets/first.png";
 const Projects = () => {
   return (
     <>
-    <Header />
-    
-    <NavBar />
-    <h2 id="language-heading">Tech Stack</h2>
+      <Header />
+      <NavBar />
+      <h2 id="language-heading">Tech Stack</h2>
       <div className="projects-container">
-        
         <ul id="language-icon">
-          
           <li id="language-icon-item">
             <i className="devicon-babel-plain colored"></i>
           </li>
@@ -26,7 +27,9 @@ const Projects = () => {
           <li id="language-icon-item">
             <i className="devicon-express-original-wordmark"></i>
           </li>
-          <li id="language-icon-item"><i className="devicon-github-original-wordmark"></i></li>
+          <li id="language-icon-item">
+            <i className="devicon-github-original-wordmark"></i>
+          </li>
           <li id="language-icon-item">
             <i className="devicon-git-plain-wordmark colored"></i>
           </li>
@@ -43,16 +46,16 @@ const Projects = () => {
             <i className="devicon-jquery-plain-wordmark colored"></i>
           </li>
           <li id="language-icon-item">
-          <i className="devicon-linux-plain"></i>
+            <i className="devicon-linux-plain"></i>
           </li>
           <li id="language-icon-item">
-          <i className="devicon-markdown-original"></i>
+            <i className="devicon-markdown-original"></i>
           </li>
           <li id="language-icon-item">
             <i className="devicon-nextjs-original-wordmark"></i>
           </li>
           <li id="language-icon-item">
-          <i className="devicon-nodejs-plain"></i>
+            <i className="devicon-nodejs-plain"></i>
           </li>
           <li id="language-icon-item">
             <i className="devicon-npm-original-wordmark colored"></i>
@@ -70,7 +73,7 @@ const Projects = () => {
             <i className="devicon-ssh-original-wordmark"></i>
           </li>
           <li id="language-icon-item">
-          <i className="devicon-tailwindcss-plain colored"></i>
+            <i className="devicon-tailwindcss-plain colored"></i>
           </li>
           <li id="language-icon-item">
             <i className="devicon-ubuntu-plain-wordmark colored"></i>
@@ -79,7 +82,7 @@ const Projects = () => {
             <i className="devicon-vuejs-plain-wordmark"></i>
           </li>
           <li id="language-icon-item">
-          <i className="devicon-visualstudio-plain colored"></i>
+            <i className="devicon-visualstudio-plain colored"></i>
           </li>
           <li id="language-icon-item">
             <i className="devicon-vscode-plain-wordmark colored"></i>
@@ -87,11 +90,31 @@ const Projects = () => {
           <li id="language-icon-item">
             <i className="devicon-webpack-plain colored"></i>
           </li>
-          <li id="language-icon-item"><i className="devicon-threejs-original-wordmark"></i></li>
-         
+          <li id="language-icon-item">
+            <i className="devicon-threejs-original-wordmark"></i>
+          </li>
         </ul>
       </div>
       Projects
+      <p>
+        A simple website created for the purpose of learning how to use React
+        Router
+      </p>
+      <a href="https://cat-react-router.netlify.app/">
+        <img src={react} alt="react" />
+      </a>
+      <a href="https://cats-strangers-things.netlify.app/">
+        <img src={mcj} alt="mcj" />
+      </a>
+      <a href="https://color-picker-cat.netlify.app/">
+        <img src={paint} alt="paint" />
+      </a>
+      <a href="https://cat-contact-list.netlify.app">
+      <img src={list} alt="list" />
+      </a>
+      <a href="https://puppy-pals-cat.netlify.app/">
+        <img src={first} alt="first" />
+      </a>
     </>
   );
 };
