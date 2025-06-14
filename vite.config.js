dd4b0cbebb1f237f8c6bfaee469692e19dba06c8
Catherine Hilton <catherine.hilton1786@gmail.com>
2025-06-14 11:25:53 -0400

    add git page

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/Portfolio/",
+  base: "https://cat-thebeesknees.github.io/Portfolio/",
   plugins: [react()],
   assetsInclude: ["**/*.docx"],
   build: {
