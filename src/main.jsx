05f4badf4e4907a61bc4c2c4806bf5d13cc0f3ab
Catherine Hilton <catherine.hilton1786@gmail.com>
2025-06-14 10:51:12 -0400

    fixed terminal cross

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//      <BrowserRouter>
+//       <Routes>
+//         <Route path="/*" element={<App />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.docx'],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  // base: "/Portfolio/",
   plugins: [react()],
-  assetsInclude: ['**/*.docx'],
-})
+  assetsInclude: ["**/*.docx"],
+  build: {
+    outDir: "dist",
+  },
+});
